45d1310615a679b026190c2adb7d78f70bbda9e2
it27427 <istiak.tushar.qtec@gmail.com>
2022-07-06 17:53:18 +0600

    add options for name

--- app.vue
+++ app.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    <h1>Mastering Nuxt 3</h1>
+  <div class="container">
+    <h1 class="title">Baby Name Generator</h1>
+    <p class="text">Choose your options and click the "Find Names" button below</p>
+
+    <div class="options">
+      <div class="option">
+        <div class="option-header">
+          <h4 class="option-title">01. Choose a gender</h4>
+        </div>
+
+        <div class="option-body">
+          <button>Male</button>
+          <button>Female</button>
+          <button>Unisex</button>
+        </div>
+      </div>
+
+      <div class="option">
+        <div class="option-header">
+          <h4 class="option-title">01. Choose the name's popularity</h4>
+        </div>
+
+        <div class="option-body">
+          <button>Trendy</button>
+          <button>Unique</button>
+        </div>
+      </div>
+
+      <div class="option">
+        <div class="option-header">
+          <h4 class="option-title">01. Choose name's length</h4>
+        </div>
+
+        <div class="option-body">
+          <button>All</button>
+          <button>Long</button>
+          <button>Short</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
